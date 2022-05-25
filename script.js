1b4d2d0ad995731b9b9ecbc1f1635439d87e6cf5
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-05-25 15:24:52 -0700

    file setup, fetch json data, display to web page

---
+++ script.js
@@ -0,0 +1,30 @@
+let library = []
+let index=1
+
+async function getLibrary(){
+    const response = await fetch("books.json")
+    if(!response.ok){
+        const message = `An error has occured: ${response.status}`
+        throw new Error(message)
+    }
+    library = await response.json()
+    return library
+}
+
+getLibrary().then(library=>{
+    // add an id number for each entry
+    library.forEach((item,index) =>{
+        item.id = index + 1;
+    })
+    console.log(library)
+    document.getElementById("container").innerHTML = `${library.map(getBookHTML).join('')}`
+
+})
+
+function getBookHTML(book){
+    return `
+    <div class="book">
+    <div class="author">${book.author}</div>
+    <div class="title">${book.title}</div>
+    </div>`
+}
